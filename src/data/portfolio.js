5df17aa349ca685536b572bacebbacde0804b85a
TemplarManatee <dylanbrad10@gmail.com>
2023-04-04 20:01:25 -0400

    .

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,56 +1,57 @@
 export default [
   // Exports a list of portfolio projects completed
   {
-    title: "WalkPerson", 
-    stack: ["JavaScript", "Spotify API", "Google Directions API"], 
-    link: "https://templarmanatee.github.io/WalkPerson/", 
-    imgUrl: "/assets/WalkPersonLogo.jpg"
+    title: "WalkPerson",
+    stack: ["JavaScript", "Spotify API", "Google Directions API"],
+    link: "https://templarmanatee.github.io/WalkPerson/",
+    imgUrl: "/assets/WalkPersonLogo.jpg",
   },
   {
-    title: "Spotter", 
-    stack: ["Node.js", "Express.js", "Handlebars"], 
-    link: "https://project-2-spotter.herokuapp.com/", 
-    imgUrl: "/assets/SpotterSplash.png"
+    title: "Spotter",
+    stack: ["Node.js", "Express.js", "Handlebars"],
+    link: "https://project-2-spotter.herokuapp.com/",
+    imgUrl: "/assets/SpotterSplash.png",
   },
   {
-    title: "Remarque", 
-    stack: ["MERN Stack", "RGL", "DaisyUi"], 
-    link: "https://remarque-bullet.herokuapp.com/", 
-    imgUrl: "https://i.imgur.com/jf0BRWg.png"
-  {
-    title: "CLI Employee Manager", 
-    stack: ["JavaScript", "SQL", "Express"], 
-    link: "https://github.com/templarmanatee/EZManager", 
-    imgUrl: "/assets/EmployeeManagerSplash.png"
-  }, 
-  {
-    title: "README Generator", 
-    stack: ["JavaScript"], 
-    link: "https://github.com/templarmanatee/GENME.md", 
-    imgUrl: "/assets/readMeGenImage.png"
+    title: "Remarque",
+    stack: ["MERN Stack", "RGL", "DaisyUi"],
+    link: "https://remarque-bullet.herokuapp.com/",
+    imgUrl: "https://i.imgur.com/jf0BRWg.png",
   },
   {
-    title: "Weather Forecaster", 
-    stack: ["JavaScript", "OpenWeather API", "Bootstrap"], 
-    link: "https://github.com/templarmanatee/fiveCast-a-forecasting-App", 
-    imgUrl: "/assets/weatherAppImage.png"
+    title: "CLI Employee Manager",
+    stack: ["JavaScript", "SQL", "Express"],
+    link: "https://github.com/templarmanatee/EZManager",
+    imgUrl: "/assets/EmployeeManagerSplash.png",
   },
   {
-    title: "JavaScript Quiz", 
-    stack: ["JavaScript", "HTML", "CSS"], 
-    link: "https://templarmanatee.github.io/A-Beginner-JavaScript-Quiz/", 
-    imgUrl: "/assets/CodingQuizImage.png"
+    title: "README Generator",
+    stack: ["JavaScript"],
+    link: "https://github.com/templarmanatee/GENME.md",
+    imgUrl: "/assets/readMeGenImage.png",
   },
   {
-    title: "Password Generator", 
-    stack: ["JavaScript", "OpenWeather API", "Bootstrap"], 
-    link: "https://github.com/templarmanatee/fiveCast-a-forecasting-App", 
-    imgUrl: "/assets/PasswordGenSplash.png"
+    title: "Weather Forecaster",
+    stack: ["JavaScript", "OpenWeather API", "Bootstrap"],
+    link: "https://github.com/templarmanatee/fiveCast-a-forecasting-App",
+    imgUrl: "/assets/weatherAppImage.png",
   },
   {
-    title: "Portfolio", 
-    stack: ["React", "Tailwind", "JavaScript"], 
-    link: "https://github.com/templarmanatee/DylanFPortfolio", 
-    imgUrl: "assets/portfolioImage.png"
+    title: "JavaScript Quiz",
+    stack: ["JavaScript", "HTML", "CSS"],
+    link: "https://templarmanatee.github.io/A-Beginner-JavaScript-Quiz/",
+    imgUrl: "/assets/CodingQuizImage.png",
+  },
+  {
+    title: "Password Generator",
+    stack: ["JavaScript", "OpenWeather API", "Bootstrap"],
+    link: "https://github.com/templarmanatee/fiveCast-a-forecasting-App",
+    imgUrl: "/assets/PasswordGenSplash.png",
+  },
+  {
+    title: "Portfolio",
+    stack: ["React", "Tailwind", "JavaScript"],
+    link: "https://github.com/templarmanatee/DylanFPortfolio",
+    imgUrl: "assets/portfolioImage.png",
   },
 ];
